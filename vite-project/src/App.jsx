393d577427cf4416ec0ce6a4480159179e9d7354
lavhalekiran <lavhalekiran46@gmail.com>
2023-09-20 19:09:18 +0530

    add

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import Login from "./pages/Login";
+
+import Navbar from './pages/Navbar';
+
+function App() {
+  
+  return (
+    
+      <div>
+        <Navbar/>
+        <Login/>
+      </div>
+      
+  )
+}
+
+export default App
---
+++ vite-project/src/pages/ChatRoom.jsx
@@ -0,0 +1,6 @@
+const ChatRoom=()=>{
+    return(
+        <div></div>
+    )
+}
+export default ChatRoom;
